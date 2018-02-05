965b9e4db342849e07ce25734ee2bb46faead5a1
Roukna Sengupta <rouknasengupta@gmail.com>
2018-02-05 10:51:54 -0500

    Changes to Department & Contributor.

--- load_grant_data_from_file.py
+++ load_grant_data_from_file.py
@@ -2,7 +2,9 @@ import sys
 import yaml
 import csv
 
-from owlery import Connection
+from vivo_connect import Connection
+from owls import match_input
+from author import Author
 import queries
 
 
@@ -53,15 +55,93 @@ def prepare_query(connection, input_file):
 
             if not response['results']['bindings']:
                 print "Grant:" + str(item.name)
-                item.awarded_by = row['Dept']
-                item.awarding_dep_type = 'Academic department'
+                item2 = params['AwardingDepartment']
+                item2.name = row['Dept']
+
+                # Check if Department exists
+                try:
+                    deets = {}
+                    search_query = "get_department_list"
+                    query_path = getattr(queries, search_query)
+                    current_list = query_path.run(connection, **deets)
+                    match = match_input(row['Dept'], current_list)
+                except Exception as e:
+                    print(e)
+                    match = 'none'
+
+                if match == 'none':
+                    try:
+                        item2.name = row['Dept']
+                        item2.dep_type = 'Academic department'
+                        params2 = {'Department': item2}
+                        update_path = getattr(queries, 'make_department')
+                        response = update_path.run(connection, **params2)
+                        print(response)
+                    except Exception as e:
+                        print("Owl Post can not create a(n) " + item2.type +
+                              " at this time. Please go to your vivo site and make it manually.")
+                else:
+                    item2.n_number = match
+                    print("The n number for this " + item2.type + "is " + item2.n_number)
+
                 item.direct_costs = row['Total_Direct']
                 item.total_award_amount = row['Total_Awarded']
                 item.name = row['Title']
-                item.contributor_role = 'Co-Principal Investigator Role'
-                item.contributor = row['PI']
+                item3 = params['Contributor']
+                item3.name = row['PI']
+
+                # Check if Contributor exists
+                try:
+                    deets = {}
+                    search_query = "get_person_list"
+                    query_path = getattr(queries, search_query)
+                    current_list = query_path.run(connection, **deets)
+                    match2 = match_input(row['PI'], current_list)
+                except Exception as e:
+                    print(e)
+                    match2 = 'none'
+
+                if match2 == 'none':
+                    try:
+                        item3.name = row['PI']
+                        item3.type = 'Co-Principal Investigator Role'
+                        author = Author(connection)
+                        author.name = row['PI']
+                        author.first = row['PI'].split(" ")[0]
+                        author.last = row['PI'].split(" ")[1]
+                        update_path2 = getattr(queries, 'make_person')
+                        params2 = {'Author': author}
+                        try:
+                            response2 = update_path2.run(connection, **params2)
+                        except Exception as e:
+                            print(e)
+                            print("Owl Post can not create a(n) " + author.type +
+                                  " at this time. Please go to your vivo site and make it manually.")
+
+                        params3 = {'Contributor': item3, 'Author': author}
+                        update_path3 = getattr(queries, 'make_contributor')
+                        response3 = update_path3.run(connection, **params3)
+                    except Exception as e:
+                        print e
+                        print("Owl Post can not create a(n) " + item3.type +
+                              " at this time. Please go to your vivo site and make it manually.")
+                else:
+                    author = Author(connection)
+                    author.n_number = match2
+                    item3.type = 'Co-Principal Investigator Role'
+                    print("The n number for this " + author.type + "is " + author.n_number)
+                    try:
+                        params3 = {'Contributor': item3, 'Author': author}
+                        update_path3 = getattr(queries, 'make_contributor')
+                        response3 = update_path3.run(connection, **params3)
+                    except Exception as e:
+                        print e
+                        print("Owl Post can not create a(n) " + item3.type +
+                              " at this time. Please go to your vivo site and make it manually.")
+
                 item.start_date = row['Budget_Begin_Date']
                 item.end_date = row['Budget_End_Date']
+                print params
                 response = template_mod.run(connection, **params)
             else:
                 # Grant already exist
@@ -77,9 +157,8 @@ def main(argv1, argv2):
     update_endpoint = config.get('update_endpoint')
     query_endpoint = config.get('query_endpoint')
     vivo_url = config.get('upload_url')
-    check_url = config.get('checking_url')
 
-    connection = Connection(vivo_url, check_url, email, password, update_endpoint, query_endpoint)
+    connection = Connection(vivo_url, email, password, update_endpoint, query_endpoint)
     prepare_query(connection, input_file)
 
 
